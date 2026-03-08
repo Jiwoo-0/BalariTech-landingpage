5c07e51c56680d3ad3eb00cf0d848a14f4c12ef0
Jiwoo-0 <ropeta.aaron@gmail.com>
2026-03-08 21:43:02 +0800

    fixed default homepage to coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Homepage from './components/Homepage'
+import Coming_soon from './components/Coming_soon'
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Homepage/>}></Route>
+          <Route path='/' element={<Coming_soon/>}></Route>
         </Routes>
       </Router>
     </>
---
+++ src/components/Coming_soon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+
+const Coming_soon = () => {
+  return (
+    <div className='flex flex-col justify-center items-center w-full h-screen bg-gradient-to-r from-[#548769] to-[#155146] '>
+      <img src={logo} alt="balariTech_logo" className='w-96'/>
+      {/* <Logo className="w-96"/> */}
+      
+      <h1 className='text-white'>IS COMING SOON</h1>
+    </div>
+  )
+}
+
+export default Coming_soon
