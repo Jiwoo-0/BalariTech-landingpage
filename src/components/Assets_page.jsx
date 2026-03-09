3d72b7d4d3e8847bc2b37364233e915cd24034dd
Jiwoo-0 <ropeta.aaron@gmail.com>
2026-03-09 17:59:53 +0800

    fix layout on asset page

--- src/components/Assets_page.jsx
+++ src/components/Assets_page.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-import vl_logo from '../assets/Vertical-Lockup.svg'
-import hl_logo from '../assets/logo.svg'
-
+import vl_logo_svg from '../assets/Vertical-Lockup.svg'
+import hl_logo_svg from '../assets/logo.svg'
+import vl_logo_png from '../assets/Vertical-Lockup.png'
 
 const Assets_page = () => {
   return (
     <div>
       <h1>Assets</h1>
-      <img src={vl_logo} alt="vertical lockup" className='w-96'/>
-      <img src={hl_logo} alt="horizontal lockup" className='w-96'/>
+      <div className='flex gap-10'>
+        <div>
+          <h3>SVG</h3>
+          <img src={vl_logo_svg} alt="vertical lockup" className='w-64'/>
+          <img src={hl_logo_svg} alt="horizontal lockup" className='w-64'/>
+        </div>
+        <div className='bg-gray-100'>
+          <h3>PNG</h3>
+          <img src={vl_logo_png} alt="horizontal lockup" className='w-64'/>
+        </div>
+      </div>
     </div>
   )
 }
